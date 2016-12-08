2a00ac7d6bad4f6b5f7e022ed26393bdab3b54e6
Albin <j.albin.willman@gmail.com>
2016-12-08 23:20:03 +0100

    Solve day 7 part 2

--- day7/abba-parser.rb
+++ day7/abba-parser.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 data = File.open('data.txt').read
-abba_count = 0
+abba_count, ssl_count = 0, 0
 
 def valid_abba?(snippets)
   can_be_valid = false
@@ -14,7 +14,7 @@ end
 
 def contains_abba?(s)
   (s.length-3).times do |i|
-    return true if is_abba_string?(s[i..(i+3)])
+    return s[i..(i+3)] if is_abba_string?(s[i..(i+3)])
   end
   false
 end
@@ -28,3 +28,40 @@ data.each_line do |line|
 end
 
 puts abba_count
+
+def valid_ssl?(snippets)
+  bab_prospects, abas = [], []
+  snippets.each_with_index do |s, i|
+    if i % 2 == 1
+      bab_prospects << s
+    else
+      abas += find_abas(s)
+    end
+  end
+  return false if abas.length == 0
+  matching_abas_and_babs?(bab_prospects, abas)
+end
+
+def matching_abas_and_babs?(bab_prospects, abas)
+  abas.each do |aba|
+    bab = "#{aba[1]}#{aba[0]}#{aba[1]}"
+    bab_prospects.each do |s|
+      return true if s.include?(bab)
+    end
+  end
+  false
+end
+
+def find_abas(s)
+  abas = []
+  (s.length-2).times do |i|
+    abas << s[i..(i+2)] if s[i] == s[i + 2] && s[i] != s[i+1]
+  end
+  abas
+end
+
+data.each_line do |line|
+  ssl_count += 1 if valid_ssl?(line.split(/\[(.*?)\]/).reject { |s| s.length == 0 })
+end
+
+puts ssl_count
