0dde3a9ed9b88a2af6bfec6a78bcc46a1ef7d819
Albin <j.albin.willman@gmail.com>
2016-12-08 23:02:55 +0100

    Solves day 5 part 2

--- day5/password-finder.rb
+++ day5/password-finder.rb
@@ -1,12 +1,44 @@
 #!/usr/bin/env ruby
 require 'digest'
 
-id, index, password = 'ugkcyxxp', 0, ''
+id, index, found, sep = 'ugkcyxxp', 0, 0, '-'
+password = sep*8
 digest = Digest::MD5.new
 
-while password.length < 8
+# while password.length < 8
+#   hex = digest.hexdigest("#{id}#{index}")
+#   password += hex[5] if hex.start_with?('00000')
+#   index += 1
+# end
+# puts password
+
+# Solution 2
+
+def numeric?(lookAhead)
+  lookAhead =~ /[0-9]/
+end
+
+def nextSep(sep)
+  return '|' if sep == '-'
+  '-'
+end
+
+while found < 8
   hex = digest.hexdigest("#{id}#{index}")
-  password += hex[5] if hex.start_with?('00000')
+  if index % 50000 === 0
+    newSep = nextSep(sep)
+    password.gsub!(/#{Regexp.quote(sep)}/, newSep)
+    sep = newSep
+
+    print '     ' + password + "\r"
+  end
+
+  if hex.start_with?('00000') && numeric?(hex[5]) && password[hex[5].to_i] == sep
+    found += 1
+    password[hex[5].to_i] = hex[6]
+    print '     ' + password + "\r"
+  end
   index += 1
 end
-puts password
+
+puts '     ' + password
