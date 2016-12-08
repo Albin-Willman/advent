77f038f6537c0ef8fa509edd4e6d3c5302b0bdfd
Albin <j.albin.willman@gmail.com>
2016-12-08 12:28:09 +0100

    Tweak day 3 for vertical triangles

--- day3/validate-triangles.rb
+++ day3/validate-triangles.rb
@@ -1,15 +1,27 @@
 #!/usr/bin/env ruby
 
-data = File.open('data.txt').read
-possible_triangles  = 0
+def new_batch
+  [[], [], []]
+end
+
+possible_triangles, triangles = 0, new_batch
 
 def valid_triangle?(trio)
   trio.map!(&:to_i).sort!
   trio[2] < (trio[1] + trio[0])
 end
 
-data.each_line do |line|
-  possible_triangles += 1 if valid_triangle?(line.split(' '))
+
+
+File.open('data.txt').read.each_line.with_index do |line, i|
+  line_data = line.split(' ')
+  line_data.each_with_index { |p, i| triangles[i] << p }
+  if i % 3 == 2
+    triangles.each do |triangle|
+      possible_triangles += 1 if valid_triangle?(triangle)
+    end
+    triangles = new_batch
+  end
 end
 
 puts possible_triangles
