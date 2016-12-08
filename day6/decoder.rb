9e746e30aec95f5081a082633608bdcb10cd7a09
Albin <j.albin.willman@gmail.com>
2016-12-08 08:11:40 +0100

    Adapt day 6 for part 2

--- day6/decoder.rb
+++ day6/decoder.rb
@@ -3,8 +3,10 @@
 data = File.open('data.txt').read
 possibilities = ['']*8
 
+FREQ_SEARCH = :min_by #max_by
+
 def find_most_frequent(arr)
-  freq = arr.reduce(Hash.new(0)) { |h, v| h[v] += 1; h }.max_by{ |k,v| v }
+  freq = arr.reduce(Hash.new(0)) { |h, v| h[v] += 1; h }.send(FREQ_SEARCH) { |k,v| v }
   freq[0]
 end
 
@@ -15,4 +17,3 @@ data.each_line do |line|
 end
 
 puts possibilities[0..7].map { |p| find_most_frequent(p.split('')) }.join
-
