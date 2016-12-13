4cc693fe8133a2c05088d97bc0a5d13e3572226e
Albin <j.albin.willman@gmail.com>
2016-12-13 11:30:44 +0100

    Day 1 2015

---
+++ 2015/day1/counter.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+File.open('data.txt').read.each_line do |line|
+  line.gsub!(/\s+/, '')
+  total_length = line.length
+  line.gsub!(/#{Regexp.quote('(')}/, '')
+  steps_down = line.length
+  puts total_length - 2*steps_down
+end
+
+File.open('data.txt').read.each_line do |line|
+  arr = line.gsub(/\s+/, '').split('')
+  counter = 0
+  arr.each_with_index do |c, i|
+    counter += (c == '(' ? 1 : -1)
+    puts counter
+    if counter == -1
+      puts i + 1
+      break
+    end
+  end
+
+end
