7241bae44ca474ff16e979087d8dbf3244e92069
Albin <j.albin.willman@gmail.com>
2016-12-20 07:48:55 +0100

    Solves day 19 fast

--- 2016/day19/elf-ring.rb
+++ 2016/day19/elf-ring.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 input = 5
-# input = 10
 # input = 3005290
 arr = (1..input).to_a
 
@@ -13,6 +12,7 @@ def reduce(arr)
   end
   reduce(arr.reject { |e| e == 0})
 end
+
 
 def linear_2(arr)
   length = arr.length
@@ -27,6 +27,30 @@ def linear_2(arr)
   arr[0]
 end
 
+100.times do |i|
+  elves = i + 1
+  arr = (1..elves).to_a
+  res = linear_2(arr)
+  last_reset, next_reset, guess = 0, 0, 0
+  15.times do |j|
+    next_reset = (3**j)
+    break if next_reset > (elves)
+    last_reset = next_reset
+  end
+  guess =  elves - last_reset + [elves*2 - last_reset - next_reset, 0].max/2
+  guess = last_reset if guess == 0
+  puts "res #{elves} -> #{res}, #{guess} "
+end
 
-# puts reduce(arr)
-# puts linear_2(arr)
+
+elf = 3005290
+last_reset, next_reset, guess = 0, 0, 0
+15.times do |j|
+  next_reset = (3**j)
+  break if next_reset > (elf)
+  last_reset = next_reset
+end
+
+guess =  elf - last_reset + [elf*2 - last_reset - next_reset, 0].max/2
+guess = last_reset if guess == 0
+puts guess
